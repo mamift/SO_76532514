c4a59a254c4b352b4c83c26e438232ed0c8d69d9
mamift <noreply@github.com>
2023-06-30 09:48:29 +1000

    Added console project for testing.

---
+++ src/ConsoleTest/Module1.vb
@@ -0,0 +1,28 @@
+﻿Imports OneTrain.infofer
+Imports System.Xml
+Imports System.Xml.Serialization
+
+Module Module1
+
+    Sub Main(args As String())
+        Dim fullPath = args.First()
+
+        Dim obj As PathDetailsMessage
+        Dim ser As New XmlSerializer(GetType(PathDetailsMessage))
+        Using reader As XmlReader = XmlReader.Create(fullPath)
+            obj = CType(ser.Deserialize(reader), PathDetailsMessage)
+        End Using
+
+        Console.WriteLine(obj)
+
+        If (obj.PathInformation Is Nothing) Then
+            Console.WriteLine("obj.PathInformation was null")
+        End If
+
+        If obj.CFRTrainInfo Is Nothing Then
+            Console.WriteLine("obj.CFRTrainInfo was null")
+        End If
+
+    End Sub
+
+End Module
---
+++ src/ConsoleTest/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ConsoleTest")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("McCullough Robertson Lawyers")>
+<Assembly: AssemblyProduct("ConsoleTest")>
+<Assembly: AssemblyCopyright("Copyright © McCullough Robertson Lawyers 2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("44f1f557-4d13-426b-82a0-6bca4ce7b58f")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
